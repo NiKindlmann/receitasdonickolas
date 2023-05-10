1ded5774d8b982d03673915c9d6f8252cd13f022
NiKindlmann <105008607+NiKindlmann@users.noreply.github.com>
2023-05-10 10:59:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <link rel="stylesheet" href="personalização.css">
   </head>
   <body>
-    <h1>MINHA PÁGINA INICIAL>
+    <h1>MINHA PÁGINA INICIAL</h1>
     <p>PÁGINA INICIAL</p>
-    <a href"#" class="button"> ENTRE EM CONTATO</a>
+    <ahref"#" class="button"> ENTRE EM CONTATO</a>
   </body>
 </html>
